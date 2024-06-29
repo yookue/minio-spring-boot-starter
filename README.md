5c9aa546df97f5d41b790546a3cb19fec2dda400
yookue <yookue@outlook.com>
2024-06-29 13:45:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Minio Spring Boot Starter
+
+Spring Boot application integrates `minio` quickly, which is a high performance object Storage.
+
+## Quickstart
+
+- Import dependencies
+
+```xml
+    <dependency>
+        <groupId>com.yookue.springstarter</groupId>
+        <artifactId>minio-spring-boot-starter</artifactId>
+        <version>LATEST</version>
+    </dependency>
+```
+
+> By default, this starter will auto take effect, you can turn it off by `spring.minio.enabled = false`
+
+- Configure Spring Boot `application.yml` with prefix `spring.minio`
+
+```yml
+spring:
+    minio:
+        host: '127.0.0.1'
+        port: 9000
+        secure-http: true
+        access-key: 'foo'
+        secret-key: 'bar'
+        bucket-name: 'app'
+```
+
+- Configure your beans with a `MinioClient` bean by constructor or `@Autowired`/`@Resource` annotation, then you can access it as you wish.
+
+## Document
+
+- Github: https://github.com/yookue/minio-spring-boot-starter
+- Minio homepage: https://min.io
+- Minio github: https://github.com/minio/minio
+
+## Requirement
+
+- jdk 1.8+
+
+## License
+
+This project is under the [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0)
+
+See the `NOTICE.txt` file for required notices and attributions.
+
+## Donation
+
+You like this package? Then [donate to Yookue](https://yookue.com/public/donate) to support the development.
+
+## Website
+
+- Yookue: https://yookue.com
---
+++ src/main/java/com/yookue/springstarter/minio/config/MinioAutoConfiguration.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2020 Yookue Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.yookue.springstarter.minio.config;
+
+
+import javax.annotation.Nonnull;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.yookue.commonplexus.springcondition.annotation.ConditionalOnAllProperties;
+import com.yookue.springstarter.minio.property.MinioProperties;
+import com.yookue.springstarter.minio.util.MinioConfigurationUtils;
+import io.minio.MinioClient;
+
+
+/**
+ * Configuration for {@link io.minio.MinioClient}
+ *
+ * @author David Hsing
+ * @reference "https://min.io/"
+ * @see io.minio.MinioClient
+ */
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnAllProperties(value = {
+    @ConditionalOnProperty(prefix = MinioAutoConfiguration.PROPERTIES_PREFIX, name = "enabled", havingValue = "true", matchIfMissing = true),
+    @ConditionalOnProperty(prefix = MinioAutoConfiguration.PROPERTIES_PREFIX, name = "host")
+})
+@ConditionalOnClass(value = MinioClient.class)
+@EnableConfigurationProperties(value = MinioProperties.class)
+@SuppressWarnings({"JavadocDeclaration", "JavadocLinkAsPlainText"})
+public class MinioAutoConfiguration {
+    public static final String PROPERTIES_PREFIX = "spring.minio";    // $NON-NLS-1$
+    public static final String MINIO_CLIENT = "minioClient";    // $NON-NLS-1$
+
+    @Bean(name = MINIO_CLIENT)
+    @ConditionalOnMissingBean(name = MINIO_CLIENT)
+    public MinioClient minioClient(@Nonnull MinioProperties properties) throws Exception {
+        return MinioConfigurationUtils.minioClient(properties);
+    }
+}
